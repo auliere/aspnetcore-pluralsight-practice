eb62c8ad32ef29e03cbb614f101123712f3c7e28
Oleg Pedorenko <olezhkoo@gmail.com>
2016-10-24 03:49:48 +0300

    Added work done to date

---
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -0,0 +1,48 @@
+﻿using OdeToFood.Entities;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace OdeToFood.Services
+{
+
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
+        Restaurant Add(Restaurant newRestaurant);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private static List<Restaurant> _restaurants;
+
+        static InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Mister Cat" },
+                new Restaurant { Id = 2, Name = "Noodle Doodle" },
+                new Restaurant { Id = 3, Name = "Mafia" }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            newRestaurant.Id = _restaurants.Max(r => r.Id) + 1;
+            _restaurants.Add(newRestaurant);
+
+            return newRestaurant;
+        }
+    }
+}
